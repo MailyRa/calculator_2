d8d7e613557c275c8d1f9250f8bfb9c4dec450de
Maily Ramos <maily.ramos1993@gmail.com>
2020-07-08 23:39:08 +0000

    Completed add function

--- calculator.py
+++ calculator.py
@@ -3,5 +3,51 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+print("Welcome to Calculator")
 
-# Replace this with your code
+#ask the user about the equation
+def calculator_2():   
+
+    user_input = (input(" Type your equation  "))
+
+    num = user_input.split(' ')
+
+#if the first letter is a "q" then I kno they want to quit 
+    if num == ["q"]:
+        print("Thank you, have a good day")
+
+#the rest of the other functions 
+    if num[0] == "+":
+        return int(num[1]) + int(num[2])
+
+    elif num[0] == "-":
+        return int(num[1]) - int(num[2])
+
+    elif num[0] == "*":
+        return int(num[1]) * int(num[2])
+
+    elif num[0] == "/":
+        return int(num[1]) / int(num[2])
+
+    elif num[0] == "square":
+        return int(num[1]) * int(num[1])
+
+    elif num[0] == "cube":
+        return int(num[1]) * int(num[1]) * int(num[1]) 
+
+    elif num[0] == "pow":
+        return int(num[1]) ** int(num[2])
+
+    elif num[0] == "mod":
+        return int(num[1]) % int(num[2])
+        
+
+print(calculator_2())
+
+
+
+
+
+
+
+
